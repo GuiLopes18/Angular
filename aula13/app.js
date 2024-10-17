ce6cd386f956925af0fa8d6a643a4fec4d5c8eb5
Guilherme Lopes <107151415+GuiLopes18@users.noreply.github.com>
2024-10-17 17:42:21 +0000

    Atividade ToDo List

---
+++ aula13/app.js
@@ -0,0 +1,25 @@
+"use strict";
+const inputElement = document.getElementById('task-input');
+const addButton = document.getElementById('add-btn');
+const taskList = document.getElementById('task-list');
+function addTask() {
+    const task = inputElement.value.trim();
+    if (task === "")
+        return;
+    const li = document.createElement('li');
+    li.textContent = task;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener('click', () => {
+        taskList.removeChild(li);
+    });
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+    inputElement.value = "";
+}
+addButton.addEventListener('click', addTask);
+inputElement.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+});
